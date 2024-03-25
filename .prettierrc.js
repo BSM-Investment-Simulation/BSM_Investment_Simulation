418464bca133f1831119d28f37d22dfc0ae29edd
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-03-25 20:38:30 +0900

    add : 프로젝트 세팅

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'all',
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  useTabs: false,
+  endOfLine: 'auto',
+  arrowParens: 'always',
+};
